c9d4fda2f55b4c9570eac9cbe3cd41a2dfb71b8d
Pedro Rezende <xpedrorezende@gmail.com>
2025-05-06 19:53:33 -0300

    fix: fixing setTimeout

--- src/heliax-custom.js
+++ src/heliax-custom.js
@@ -1,7 +1,7 @@
-requestAnimationFrame(() => {
+setTimeout(() => {
 	let $nftButton = $(`<div class="nft-button button-large"><button>Submit as Intent NFT</button></div>`);
 	$nftButton.on("click", () => {
 		window.systemHooks.onNftSubmit(window.$canvas);
 	});
 	window.$bottom.append($nftButton);
-});
+}, 600);
